327f7b76e5c2c39f91eeacd5fc26a939b7431aca
Auer <anna-maria.auer@capgemini.com>
2021-01-20 15:46:59 +0100

    mouse disappears

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
-import { Attendees } from './components/Attendees';
+import { Attendees, useMove } from './components/Attendees';
 
 function App() {
+  const {handleMouseMove, x , y} = useMove()
+
+  //console.log(x, y)
+
+  const trans = 'translate(' + x + 'px, ' + y + 'px)'
+  //const trans = 'translate(' + 10 + 'px, ' + 20 + 'px)'
+  const style = {
+      transform: trans
+  }
+
+  console.log(style)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleMouseMove()
+    }, 2000)
+  })
+
   return (
+    <div>
     <div className="App">
+      
       <header>
         <h1>Accessability Training</h1>
       </header>
-      <Attendees></Attendees>
+      <Attendees ></Attendees>
+    </div>
+    <svg viewBox="11.8 9 16 22"><path d="M20,21l4.5,8l-3.4,2l-4.6-8.1L12,29V9l16,12H20z"
+                style={style}></path></svg> 
     </div>
   );
 }
--- src/components/Attendees.tsx
+++ src/components/Attendees.tsx
@@ -1,6 +1,8 @@
 import React, { FormEvent, useEffect, useState } from "react"
 import { useAttendees } from "./AttendeeHook"
 import { NewAttendeeForm } from "./NewAttendeeForm"
+import moment from 'moment'
+const SimplexNoise = require('simplex-noise')
 
 export type Attendee = {
     id: string,
@@ -9,6 +11,40 @@ export type Attendee = {
     street_number?: string,
     zipcode?: string,
     city?: string
+}
+
+
+
+export const useMove = () => {
+
+    const [fakeMousePosition, setMousePosition] = useState({x: 0 , y: 0})
+    const simplex = new SimplexNoise(Math.random)
+
+    const handleMouseMove = () => {
+        const ms = moment().valueOf()
+        const s = 0.001 * (20 / 100);
+        const noiseX = (simplex.noise3D(1, 0, s * ms) + 1) / 2;
+        const noiseY = (simplex.noise3D(11, 0, s * ms) + 1) / 2;
+
+        const random = 40/1000
+
+        const innerWidth =  window.innerWidth
+        const innerHeight = window.innerHeight
+        
+        const randX = simplex.noise3D(1, 0, random) * innerWidth * 0.1;
+        const randY = simplex.noise3D(3, 0, random) * innerHeight * 0.1;
+
+        const x = noiseX * innerWidth + randX
+        const y = noiseY * innerHeight + randY
+
+        setMousePosition({x: x, y: y})
+
+    }
+    return {
+        x: fakeMousePosition.x,
+        y: fakeMousePosition.y,
+        handleMouseMove
+    }
 }
 
 export const Attendees = () => {
